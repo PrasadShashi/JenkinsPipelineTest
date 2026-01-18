b82d9ba4a1ae6b5ed49da08d65921e154e5c4b7e
PrasadShashi <prasadshashi671@gmail.com>
2026-01-18 23:25:32 +0530

    Java program for Jenkins build

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class TestJava{
+    public static void main(String[] args) {
+        System.out.println("Hello this is a Testing code for Jenkins Build");
+        System.out.println("This files will be used in jenkins pipeline");
+        System.out.println("Date:"+ new java.util.Date());
+    
+    }
+}
